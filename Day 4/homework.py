37ab138f0b18aba78addbf8f583c20cb484f0edf
nikakandelaki <139273739+nikakandelaki@users.noreply.github.com>
2023-07-18 14:44:25 +0400

    Add files via upload

---
+++ Day 4/homework.py
@@ -0,0 +1,7 @@
+user_age = input("How old are you? ")
+
+years_to_travel = input("How many years do you want to time travel? ")
+
+age_after_time_travel = user_age + years_to_travel
+
+print("After time travel, you will be", age_after_time_travel, "years old.")
