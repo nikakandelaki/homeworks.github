eebda29b163c9feba4dc3e65655dea7ed7105760
nikakandelaki <139273739+nikakandelaki@users.noreply.github.com>
2023-08-19 13:31:21 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,11 +1,6 @@
-for i in range(1,100):
-    if i % 3 == 0:
-     print("goa")
+list = ["ნიკა","ალექსანდრე","ანდრია"]
+list2 =["კანდელაკი", "კანდელაკი", "კანდელაკი"]
 
-for i in range(1,100):
-   if i % 5 == 0:
-          print("goa11")
-
-for i in range(1,100):
-    if i % 15 == 0:
-            print("goa15")
+print(list[0] + " " + list2[0])
+print(list[1] + " " + list2[1])
+print(list[2] + " " + list2[2])
